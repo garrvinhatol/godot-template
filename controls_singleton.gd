3dafe86c39ef03cfd7efce4d33dba8c885a2cad6
b1n <garrvinhatol@gmail.com>
2025-05-07 21:22:28 +0800

    again, forgot to save cotrols singleton file

--- controls_singleton.gd
+++ controls_singleton.gd
@@ -8,15 +8,6 @@ func _ready() -> void:
 	EventBus.controller_singleton = self
 	drag.started.connect(start_drag)
 	drag.completed.connect(release_drag)
-	#sends signals to event bus for inputs
-	#sends signal to event bus of objects it detects.
-	#event bus will check positions of items
-		#draggables update the event_bus when they are ready
-	pass
-
-func _process(_delta: float) -> void:
-	# EventBus.control_position = control_position.value_axis_2d
-	# print(control_position.value_axis_2d)
 	pass
 
 func start_drag():
